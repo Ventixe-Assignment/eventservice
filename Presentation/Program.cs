89a7e7b5df167b078aad869402bf8c215d067682
Janne <tapioninetytwo@live.se>
2025-05-18 22:06:01 +0200

    Added swaggerUI

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -7,6 +7,7 @@ using Presentation.Services;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 
 
 builder.Services.AddDbContext<DataContext>(o => o.UseSqlServer(builder.Configuration.GetConnectionString("EventDBConnection")));
@@ -15,6 +16,14 @@ builder.Services.AddScoped<IEventService,EventService>();
 
 var app = builder.Build();
 app.MapOpenApi();
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "EventService API");
+    c.RoutePrefix = string.Empty;
+});
+
 app.UseHttpsRedirection();
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 
